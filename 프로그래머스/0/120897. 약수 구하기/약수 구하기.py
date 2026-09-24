c41e5a5cbbc33404ce5eecdd6fc31909646b6fae
subeen <sju081902@gmail.com>
2026-09-24 12:27:23 +0900

    [level 0] Title: 약수 구하기, Time: 0.31 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
@@ -2,7 +2,7 @@ def solution(n):
     answer = []
     
     for i in range(1, n + 1):
-        if n % i == 0:  # 중앙값까지 배열
+        if n % i == 0:  # i가 n의 약수일 경우
             answer.append(i)
     
     return answer
